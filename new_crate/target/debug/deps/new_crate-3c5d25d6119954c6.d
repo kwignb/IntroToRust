df89dabc0abc24838b4a5e9c25f357093bc01294
kwignb <tkzn152131.kw@gmail.com>
2022-04-07 23:46:51 +0900

    add async-trait and module

---
+++ new_crate/target/debug/deps/new_crate-3c5d25d6119954c6.d
@@ -0,0 +1,5 @@
+/Users/kaitowatanabe/Projects/Rust/test_project/new_crate/target/debug/deps/new_crate-3c5d25d6119954c6: src/main.rs
+
+/Users/kaitowatanabe/Projects/Rust/test_project/new_crate/target/debug/deps/new_crate-3c5d25d6119954c6.d: src/main.rs
+
+src/main.rs:
---
+++ new_crate/target/debug/deps/new_crate-fa148f2cf99bb349.d
@@ -0,0 +1,11 @@
+/Users/kaitowatanabe/Projects/Rust/test_project/new_crate/target/debug/deps/new_crate-fa148f2cf99bb349.rmeta: src/lib.rs src/module_a.rs src/module_b.rs src/module_b/module_c.rs src/module_b/module_d.rs
+
+/Users/kaitowatanabe/Projects/Rust/test_project/new_crate/target/debug/deps/libnew_crate-fa148f2cf99bb349.rlib: src/lib.rs src/module_a.rs src/module_b.rs src/module_b/module_c.rs src/module_b/module_d.rs
+
+/Users/kaitowatanabe/Projects/Rust/test_project/new_crate/target/debug/deps/new_crate-fa148f2cf99bb349.d: src/lib.rs src/module_a.rs src/module_b.rs src/module_b/module_c.rs src/module_b/module_d.rs
+
+src/lib.rs:
+src/module_a.rs:
+src/module_b.rs:
+src/module_b/module_c.rs:
+src/module_b/module_d.rs:
